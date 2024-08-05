79319733db770a60fa100a09112149b533d3be1f
scubade <guilhermedelucas@yahoo.com>
2024-08-05 18:01:16 +0200

    Updates

---
+++ _app/immutable/chunks/paths.C2lWnJz9.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_19hjexn)==null?void 0:s.base)??"/moai-the-lobbyist--project";var e;const a=((e=globalThis.__sveltekit_19hjexn)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.JmSf-w8_.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.C1Hzotns.js";export{t as start};
