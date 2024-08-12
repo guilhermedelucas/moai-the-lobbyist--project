ee036881a39001b6863f532911eebe7b9457adf9
scubade <guilhermedelucas@yahoo.com>
2024-08-12 19:22:06 +0200

    Updates

--- _app/immutable/chunks/paths.BmNUlaxX.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_19k8vnb)==null?void 0:s.base)??"/moai-the-lobbyist--project";var t;const a=((t=globalThis.__sveltekit_19k8vnb)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ _app/immutable/entry/start.CypvGbB9.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D5L24pTX.js";export{t as start};
